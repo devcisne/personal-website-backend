53f6158f71def4dd4946feebbc04f54950ed30b5
Diego Cisneros <devcisne@gmail.com>
2022-08-28 20:55:28 +0200

    continued work, completed api endpoints and db logic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import creds from './creds.js';
 import nodemailer from 'nodemailer'
+import { MongoClient } from 'mongodb';
 import { check, validationResult } from 'express-validator'
 
 
@@ -14,14 +15,98 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+const withDB = async (operations, res) => {
+  const dbName = 'personalwebsite';
+  const url = 'mongodb://localhost:27017';
+
+
+  try {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true });
+    const db = client.db(dbName);
+    const collection = db.collection('blogEntries');
+
+
+    await operations(collection);
+    client.close();
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ "msg": `The following error was found with the DB operation: ${error}` })
+  }
+}
+
 
 const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Ass ree");
-});
+// app.get("/api/", (req, res) => {
+//   res.send("root api route");
+// });
+
+app.post("/api/insertMany", (req, res) => {
+  withDB(async (collection) => {
+    const entries = req.body.blogEntries
+    await collection.insertMany(entries)
+      .then((insertResult) => {
+        res.status(200).json(insertResult)
+        console.log(insertResult)
+      })
+      .catch((error) => {
+        console.error(error)
+        res.status(500).json({ "msg": `The following error was found: ${error}` })
+      })
+  }, res)
+
+})
+
+app.get("/api/blogEntries/:id", (req, res) => {
+  withDB(async (collection) => {
+    const ID = req.params.id
+    await collection.findOne({ entryID: ID })
+      .then((blogEntry) => {
+        res.status(200).json(blogEntry)
+        console.log(blogEntry)
+      })
+      .catch((error) => {
+        console.error(error)
+        res.status(500).json({ "msg": `The following error was found: ${error}` })
+      })
+  }, res)
+
+})
+
+app.get("/api/blogEntries/", (req, res) => {
+  withDB(async (collection) => {
+    await collection.find().toArray()
+      .then((blogEntryArray) => {
+        res.status(200).json(blogEntryArray)
+        console.log(blogEntryArray)
+      })
+      .catch((error) => {
+        console.error(error)
+        res.status(500).json({ "msg": `The following error was found: ${error}` })
+      })
+  }, res)
+
+})
+
+app.post("/api/blogEntries/:id/add-comment", (req, res) => {
+  const { userName, commentContent } = req.body;
+  const ID = req.params.id;
+  withDB(async (collection) => {
+    const blogEntry = await collection.findOne({ entryID: ID })
+    await collection.updateOne({ entryID: ID }, {
+      '$set': {
+        comments: blogEntry.comments.concat({ userName, commentContent }),
+      }
+    })
+    const updatedEntry = await collection.findOne({ entryID: ID })
+    console.log(updatedEntry)
+    res.status(200).json(updatedEntry)
+
+  }, res)
+
+})
 
 app.post("/sendMail", [check('email').isEmail()], (req, res) => {
   console.log(req.body)
