f4a097d7c7a31f97ffc1629e304350d4b4ce8497
Diego Cisneros <devcisne@gmail.com>
2022-08-29 23:40:14 +0200

    reformated code

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import nodemailer from 'nodemailer'
-import { MongoClient } from 'mongodb';
-import { check, validationResult } from 'express-validator'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import { MongoClient } from "mongodb";
+import { check, validationResult } from "express-validator";
 import axios from "axios";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   auth: {
     user: process.env.USER,
-    pass: process.env.PASS
-  }
+    pass: process.env.PASS,
+  },
 });
 
 const withDB = async (operations, res) => {
-  const dbName = 'personalwebsite';
-  const url = 'mongodb://localhost:27017';
-
+  const dbName = "personalwebsite";
+  const url = "mongodb://127.0.0.1:27017";
 
   try {
     const client = await MongoClient.connect(url, { useNewUrlParser: true });
     const db = client.db(dbName);
-    const collection = db.collection('blogEntries');
-
+    const collection = db.collection("blogEntries");
 
     await operations(collection);
     client.close();
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ "msg": `The following error was found with the DB operation: ${error}` })
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        msg: `The following error was found with the DB operation: ${error}`,
+      });
   }
-}
-
+};
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 // app.get("/api/", (req, res) => {
@@ -47,85 +48,94 @@ app.use(bodyParser.json());
 
 app.post("/api/insertMany", (req, res) => {
   withDB(async (collection) => {
-    const entries = req.body.blogEntries
-    await collection.insertMany(entries)
+    const entries = req.body.blogEntries;
+    await collection
+      .insertMany(entries)
       .then((insertResult) => {
-        res.status(200).json(insertResult)
-        console.log(insertResult)
+        res.status(200).json(insertResult);
+        console.log(insertResult);
       })
       .catch((error) => {
-        console.error(error)
-        res.status(500).json({ "msg": `The following error was found: ${error}` })
-      })
-  }, res)
-
-})
+        console.error(error);
+        res
+          .status(500)
+          .json({ msg: `The following error was found: ${error}` });
+      });
+  }, res);
+});
 
 app.get("/api/blogEntries/:id", (req, res) => {
   withDB(async (collection) => {
-    const ID = req.params.id
-    await collection.findOne({ entryID: ID })
+    const ID = req.params.id;
+    await collection
+      .findOne({ entryID: ID })
       .then((blogEntry) => {
-        res.status(200).json(blogEntry)
-        console.log(blogEntry)
+        res.status(200).json(blogEntry);
+        console.log(blogEntry);
       })
       .catch((error) => {
-        console.error(error)
-        res.status(500).json({ "msg": `The following error was found: ${error}` })
-      })
-  }, res)
-
-})
+        console.error(error);
+        res
+          .status(500)
+          .json({ msg: `The following error was found: ${error}` });
+      });
+  }, res);
+});
 
 app.get("/api/blogEntries/", (req, res) => {
   withDB(async (collection) => {
-    await collection.find().toArray()
+    await collection
+      .find()
+      .toArray()
       .then((blogEntryArray) => {
-        res.status(200).json(blogEntryArray)
-        console.log(blogEntryArray)
+        res.status(200).json(blogEntryArray);
+        console.log(blogEntryArray);
       })
       .catch((error) => {
-        console.error(error)
-        res.status(500).json({ "msg": `The following error was found: ${error}` })
-      })
-  }, res)
-
-})
+        console.error(error);
+        res
+          .status(500)
+          .json({ msg: `The following error was found: ${error}` });
+      });
+  }, res);
+});
 
 app.post("/api/blogEntries/:id/add-comment", (req, res) => {
   const { userName, commentContent } = req.body;
   const ID = req.params.id;
   withDB(async (collection) => {
-    const blogEntry = await collection.findOne({ entryID: ID })
-    await collection.updateOne({ entryID: ID }, {
-      '$set': {
-        comments: blogEntry.comments.concat({ userName, commentContent }),
+    const blogEntry = await collection.findOne({ entryID: ID });
+    await collection.updateOne(
+      { entryID: ID },
+      {
+        $set: {
+          comments: blogEntry.comments.concat({ userName, commentContent }),
+        },
       }
-    })
-    const updatedEntry = await collection.findOne({ entryID: ID })
-    console.log(updatedEntry)
-    res.status(200).json(updatedEntry)
-
-  }, res)
-
-})
+    );
+    const updatedEntry = await collection.findOne({ entryID: ID });
+    console.log(updatedEntry);
+    res.status(200).json(updatedEntry);
+  }, res);
+});
 
 app.post("/api/verifyCaptcha", (req, res) => {
-  console.log("token", req.body.token)
-  axios({ method: "POST", url: `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.VERIFY_SECRET}&response=${req.body.token}` })
+  console.log("token", req.body.token);
+  axios({
+    method: "POST",
+    url: `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.VERIFY_SECRET}&response=${req.body.token}`,
+  })
     .then((response) => {
-      console.log("Response success?", response.data.success)
-      res.status(200).json({ success: response.data.success })
-    }).catch((error) => {
+      console.log("Response success?", response.data);
+      res.status(200).json({ success: response.data.success });
+    })
+    .catch((error) => {
       console.log(`Error with recaptcha req:`, error);
     });
+});
 
-})
-
-
-
-app.post("/api/sendMail", [check('email').isEmail()], (req, res) => {
-  console.log(req.body)
+app.post("/api/sendMail", [check("email").isEmail()], (req, res) => {
+  console.log(req.body);
 
   const output = `
   <p>You have a new contact request</p>
@@ -135,28 +145,31 @@ app.post("/api/sendMail", [check('email').isEmail()], (req, res) => {
   <li>Email: ${req.body.email}</li>
   </ul>
   <h3>Message:</h3>
-  <p>${req.body.msg || '[No message]'}</p>
+  <p>${req.body.msg || "[No message]"}</p>
   `;
 
   const mailOptions = {
     from: `"Diego's website" <${process.env.USER}>`,
     to: `${process.env.USER}`,
-    subject: req.body.subject || '[No subject]',
-    html: output || '[No message]'
+    subject: req.body.subject || "[No subject]",
+    html: output || "[No message]",
   };
 
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).send({ message: 'Something is wrong with the request...' });
+    return res
+      .status(400)
+      .send({ message: "Something is wrong with the request..." });
   }
 
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) return res.status(500).send(err);
-    console.log('Message sent: %s', info);
+    console.log("Message sent: %s", info);
   });
-  res.status(200).json({ "msg": `${req.body.senderName} your contact message has been sent` });
-
+  res
+    .status(200)
+    .json({ msg: `${req.body.senderName} your contact message has been sent` });
 });
 
 app.listen(5000, () => {
